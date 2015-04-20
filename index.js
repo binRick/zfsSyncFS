fce700157a23b1640c5ffbcb3776345419c689df
binRick <rapidvps@gmail.com>
2015-04-20 14:41:11 -0400

    i

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 #!/usr/bin/env node
 
+//# export FS="aPool/9894631" && export REMOTE="crater" && alias i="./index.js" && alias p="prettyjson" && alias o="i | p"
+var pb = require('pretty-bytes'),
+    ta = require('time-ago'),
+    ago = require('ago'),
+    POOL = 'tank',
+    moment = require('moment');
+
 var pj = require('prettyjson'),
     _ = require('underscore'),
     async = require('async'),
     c = require('chalk'),
     zfs = require(__dirname + '/node-zfs').zfs,
+    zpool = require(__dirname + '/node-zfs').zpool,
     s = require('underscore.string'),
-    fs = process.argv[2] || 't/Rick',
-    remote = process.argv[3] || 'beo',
+    fs = process.env.FS || process.argv[2] || 't/Rick',
+    remote = process.env.REMOTE || process.argv[3] || 'beo',
     local = require('os').hostname().split('.')[0],
     Client = require('ssh2').Client,
     conn = new Client();
 
+var normalizePoolVals = function(a, b) {
+    var i, index, V = 0,
+        c = {};
+    for (V in b)
+        for (i in a)
+            c[a[i]] = b[V][i];
+    return c;
+};
 
-var remotePath = 'tank/Snapshots/' + local;
+var allZfsFields = ['all'],
+    customFields = [{
+        field: '_backup_interval',
+        default: 84600
+    }],
+    zfsFields = ['used', 'usedbysnapshots', 'atime', 'compressratio', 'logicalreferenced', 'mountpoint', 'mounted', 'creation', 'refcompressratio', 'available', 'referenced', 'dedup', 'logicalused', 'type', 'usedbydataset', 'creation', 'quota'];
+var zfsFieldsPlusCustom = zfsFields.concat(customFields.map(function(f) {
+        return 'custom:' + f.field;
+    })),
+    remotePath = 'tank/Snapshots/' + local;
 
-
-var SnapFilter = function(l) {
+var SnapshotsParser = function(fs, cb) {
+        return zfs.get(fs, zfsFields, [], cb);
+    },
+    SnapFilter = function(l) {
         return l.name.split('@')[0] == fs;
     },
     RemoteSnapFilter = function(l) {
@@ -56,42 +83,13 @@ var getRemoteSnapshots = function(cb) {
 };
 
 var getLocalSnapshots = function(cb) {
-    /*
-     *
-     *
-     *      written: '1189376',
-     *           logicalused: '2784256',
-     *                logicalreferenced: '2724864',
-     *
-     *
-     *      usedbysnapshots: '143360',
-     *           usedbydataset: '3096576',
-     *                usedbychildren: '0',
-     *
-     *    { type: 'filesystem',
-     *         creation: '1427498900',
-     *              used: '150056960',
-     *                   available: '52122885120',
-     *                        referenced: '149904896',
-     *
-     *      recordsize: '131072',
-     *           mountpoint: '/t/Rick',
-     *
-     *      mounted: 'yes',
-     *           quota: '0',
-     *
-     * */
-    zfs.get(fs, ['all'], [], function(e, i) {
-        if (e) throw e;
-        //       console.log(i);
-    });
     zfs.list_snapshots(function(err, fields, list) {
         if (err) throw err;
-        list = list.map(function(l, index, arr) {
+        var Snapshots = list.map(function(l, index, arr) {
             var P = arr[index - 1] || [];
             P = P[0] || '';
             P = P.split('@')[1] || '';
-
+            if (index == 0) P = '';
             return {
                 name: l[0],
                 used: l[1],
@@ -99,12 +97,13 @@ var getLocalSnapshots = function(cb) {
                 refer: l[3],
                 type: l[4],
                 mountpoint: l[5],
-                prevSnap: P,
+                SnapshotListIndex: index,
             };
         }).filter(SnapFilter);
-        cb(null, {
+        var e = null;
+        cb(e, {
             Type: 'Local',
-            Data: list
+            Data: Snapshots
         });
     });
 };
@@ -135,12 +134,28 @@ async.parallel([getLocalSnapshots, getRemoteSnapshots], function(err, Snapshots)
     Snaps.toSync = Snaps.Local.filter(function(s) {
         return _.contains(Snaps.toSyncNames, s.name.split('@')[1]);
     });
-    Snaps.SyncCommands = Snaps.toSync.map(function(s) {
-        if (s.prevSnap.length > 0)
-            s.prevSnap = ' -i ' + s.prevSnap;
-        return 'zfs send ' + s.prevSnap + ' ' + s.name + ' | pv | ssh ' + remote + ' zfs recv -vF ' + remotePath + '/' + fs;
+    Snaps.SyncCommands = Snaps.toSync.map(function(s, i, ar) {
+        s.pvOptions = s.pvOptions || '';
+        if (s.LocalIndex == 0)
+            p.prevSnap = '';
+        return 'zfs send ' + s.prevSnap + ' ' + s.name + ' | pv ' + s.pvOptions + ' | ssh ' + remote + ' zfs recv -vF ' + remotePath + '/' + fs;
     });
-
-    console.log(Snaps);
-    if (err) throw err;
+    Snaps.fsInfo = {};
+    zpool.list(POOL, ['all'], function(e, poolFields, poolValues) {
+        if (e) throw e;
+        Snaps.poolInfo = normalizePoolVals(poolFields, poolValues);
+        SnapshotsParser(fs, function(e, lInfo) {
+            if (e) throw e;
+            Snaps.fsInfo.Local = lInfo[fs];
+            Snaps.fsSummary = {
+                Bytes: Snaps.fsInfo.Local.used,
+                Size: pb(parseInt(Snaps.fsInfo.Local.used)),
+                Snapshots: Snaps.Local.length,
+                CreationTs: parseInt(Snaps.fsInfo.Local.creation),
+                Creation: moment.unix(Snaps.fsInfo.Local.creation),
+                CTID: '12345',
+            };
+            console.log(JSON.stringify(Snaps))
+        });
+    });
 });
